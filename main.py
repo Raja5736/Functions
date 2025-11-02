045e3f0203b896a023483c13289fb99944846e92
Raja5736 <huzaifa573123@gmail.com>
2025-11-02 10:10:50 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+#Write a Python program that takes a name as an input from the user and then creates a function that accepts the same name as a parameter and greets the user.
+def greet(name):
+    print("Hi "+name+" I hope you are doing well" )
+name=input("Enter your name") 
+greet(name)
